009269d5386bec22243397d4b846e7ff839980cd
Juan Arevalo <jarevaloferrufino@gmail.com>
2025-10-15 22:19:23 -0400

    fix: virtual list error and reset pin attempts

--- src/container/TabBar/Recovery/RecoveryFinalize.js
+++ src/container/TabBar/Recovery/RecoveryFinalize.js
@@ -17,6 +17,7 @@ import wira from 'wira-sdk';
 
 
 import {useNavigationLogger} from '../../../hooks/useNavigationLogger';
+import { resetAttempts } from '../../../utils/PinAttempts';
 export default function RecoveryFinalize({route, navigation}) {
   const dispatch = useDispatch();
   const {originalPin, recData} = route.params;
@@ -26,9 +27,10 @@ export default function RecoveryFinalize({route, navigation}) {
     (async () => {
       try {
         const data = JSON.parse(recData);
-        await (new wira.RecoveryService()).saveRecoveryDataFromGuardians(data, originalPin, PROVIDER_NAME)
+        await (new wira.RecoveryService()).saveRecoveryDataFromGuardians(data, originalPin, PROVIDER_NAME);
         await AsyncStorage.removeItem(GUARDIAN_RECOVERY_DNI);
         await AsyncStorage.setItem(PENDINGRECOVERY, 'false');
+        await resetAttempts();
         dispatch(setSecrets(data));
         await startSession(null);
         navigation.navigate(AuthNav.LoginUser);
