2b5f5ada135ab772fd574a05466b0aba08f7ada1
Juan Arevalo <jarevaloferrufino@gmail.com>
2025-10-17 19:28:25 -0400

    fix: import.meta bundle error

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    // Replace import.meta with a plain object (works in release/Hermes)
+    require('./babel/plugins/transformImportMetaToObject'),
+
     [
       'module:react-native-dotenv',
       {
@@ -9,6 +12,7 @@ module.exports = {
       },
     ],
     ['@babel/plugin-transform-export-namespace-from', { allowNamespaces: true }],
+    ['babel-plugin-transform-import-meta', { module: 'ES6' }],
     'react-native-reanimated/plugin', // SIEMPRE el último
   ],
   env: {
