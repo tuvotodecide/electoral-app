6698fcfecbab929129e472d0e40c37114c989f2b
Juan Arevalo <jarevaloferrufino@gmail.com>
2025-10-19 09:56:09 -0400

    fix: location distance

--- src/container/Vote/common/ElectoralLocations.js
+++ src/container/Vote/common/ElectoralLocations.js
@@ -156,7 +156,7 @@ const ElectoralLocations = ({navigation, route}) => {
     try {
       setLoading(true);
       const response = await axios.get(
-        `${BACKEND_RESULT}/api/v1/geographic/electoral-locations/nearby?lat=${latitude}&lng=${longitude}&maxDistance=10000`,
+        `${BACKEND_RESULT}/api/v1/geographic/electoral-locations/nearby?lat=${latitude}&lng=${longitude}&maxDistance=500`,
         //`${BACKEND_RESULT}/api/v1/geographic/electoral-locations/nearby?lat=-16.4940642&lng=-68.1598532&maxDistance=10000`,
         {timeout: 15000}, // 10 segundos timeout
       );
